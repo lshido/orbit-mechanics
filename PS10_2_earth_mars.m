1d6464aa851f125299273ae19f02a1f6c7814fcd
bluelill <lillian.shido@gmail.com>
2020-12-06 22:12:33 -0800

    Adds hyperbolic velocity at periapsis for PS10 problem 2

--- PS10_2_earth_mars.m
+++ PS10_2_earth_mars.m
@@ -1,5 +1,11 @@
 mu_sun = 132712440017.99;
+mu_earth = 398600.4415;
+mu_mars = 42828.314258067;
+
 R_sun = 695990;
+R_earth = 6378.1363;
+R_mars = 3397;
+
 a_earth = 149597898;
 a_mars = 227944135;
 TA = 120;
@@ -81,13 +87,13 @@ fprintf("rp: %.4e km\n", rp);
 fprintf("rp: %.4e R_sun\n", rp/R_sun);
 fprintf("ra: %.4e km\n", ra);
 
-fprintf("-------Problem 1a: Calculate velocities-----------\n")
+fprintf("-------Problem 2b: Calculate velocities-----------\n")
 v_dep = sqrt(2*(energy + (mu_sun/r1)));
 v_arr = sqrt(2*(energy + (mu_sun/r2)));
 fprintf("v_dep: %.4e km/s\n", v_dep);
 fprintf("v_arr: %.4e km/s\n", v_arr);
 
-fprintf("-------Problem 1a: Calculate true anomalies-----------\n")
+fprintf("-------Problem 2b: Calculate true anomalies-----------\n")
 theta_star_dep_1 = acosd((p-r1)/(r1*e));
 theta_star_dep_2 = 360 - theta_star_dep_1;
 theta_star_arr_1 = acosd((p-r2)/(r2*e));
@@ -97,13 +103,13 @@ fprintf("theta_star_dep_2: %.4e deg\n", theta_star_dep_2);
 fprintf("theta_star_arr_1: %.4e deg\n", theta_star_arr_1);
 fprintf("theta_star_arr_2: %.4e deg\n", theta_star_arr_2);
 
-fprintf("-------Problem 1a: Calculate FPAs-----------\n")
+fprintf("-------Problem 2b: Calculate FPAs-----------\n")
 FPA_dep = acosd(sqrt(p*mu_sun)/(r1*v_dep));
 FPA_arr = acosd(sqrt(p*mu_sun)/(r2*v_arr));
 fprintf("FPA_dep: %.4e deg\n", FPA_dep);
 fprintf("FPA_arr: %.4e deg\n", FPA_arr);
 
-fprintf("-------Problem 1a: Calculate delta_v-----------\n")
+fprintf("-------Problem 2c: Calculate delta_v-----------\n")
 v1 = sqrt(mu_sun/r1);
 v2 = sqrt(mu_sun/r2);
 delta_v1 = sqrt((v1^2)+(v_dep^2)-(2*v1*v_dep*cosd(FPA_dep)));
@@ -131,3 +137,69 @@ fprintf("kappa_angle_2: %.4e deg\n", kappa_angle_2);
 fprintf("kappa_angle_2_check: %.4e deg\n", kappa_angle_2_check);
 fprintf("alpha_angle_2: %.4e deg\n", alpha_angle_2);
 fprintf("delta_v2_vector: %.4e V^ %.4e B^ km/s\n", delta_v2_vector);
+
+fprintf("-------Problem 2f: Consider Earth Local Field-----------\n")
+r_pass_earth = 1000 + R_earth;
+v_inf_earth = delta_v1;
+energy_hyp_earth = v_inf_earth^2/2;
+v_pass_earth = sqrt(2*(energy_hyp_earth + (mu_earth/r_pass_earth)));
+fprintf("energy_hyp_earth: %.4e km/s\n", energy_hyp_earth);
+fprintf("v_pass_earth: %.4e km/s\n", v_pass_earth);
+
+
+fprintf("-------Problem 2f: Consider Mars Local Field-----------\n")
+r_pass_mars = 500 + R_mars;
+v_arr_vector = [ v_arr 0 ];
+v_mars_vector = [ v2*cosd(FPA_arr) v2*sind(FPA_arr) ];
+v_inf_mars_vector = v_arr_vector - v_mars_vector;
+v_inf_mars = norm(v_inf_mars_vector);
+energy_hyp_mars = v_inf_mars^2/2;
+v_pass_mars = sqrt(2*(energy_hyp_mars + (mu_mars/r_pass_mars)));
+
+fprintf("v_arr_vector: %.4e V^ %.4e B^ km/s\n", v_arr_vector);
+fprintf("v_mars_vector: %.4e V^ %.4e B^ km/s\n", v_mars_vector);
+fprintf("v_inf_mars_vector: %.4e V^ %.4e B^ km/s\n", v_inf_mars_vector);
+fprintf("v_inf_mars: %.4e km/s\n", v_inf_mars);
+fprintf("energy_hyp_mars: %.4e km/s\n", energy_hyp_mars);
+fprintf("v_pass_mars: %.4e km/s\n", v_pass_mars);
+
+
+
+
+% plot earth's orbit
+p_earth = a_earth;
+plot_eph(0, p_earth, 0, 0, 360)
+
+% plot mars' orbit
+p_mars = a_mars;
+plot_eph(0, p_mars, 0, 0, 360)
+
+% plot transfer
+plot_eph(e, p, 0, 14.22, 134.22)
+
+
+
+% plot function
+function plot_eph(e,p,AOP, start_range, end_range)
+    %orbit distance
+    ta = [start_range:0.1:end_range];
+    rmag = p ./ (1+e*cosd(ta-AOP));
+    rplot(1,:) = rmag.*cosd(ta);
+    rplot(2,:) = rmag.*sind(ta);
+    
+    plot(0,0,'.','MarkerSize',10,'Color','k','HandleVisibility','off'), hold on %Primary Body
+    [~,indx] = min(abs(ta-AOP));
+    plot(rplot(1,indx),rplot(2,indx),'.','MarkerSize',7,'Color','k','HandleVisibility','off') %line of apsides
+    [~,indx] = min(abs(ta-(180+AOP)));
+    plot(rplot(1,indx),rplot(2,indx),'.','MarkerSize',7,'Color','k','HandleVisibility','off') %line of apsides
+    plot(rplot(1,:),rplot(2,:)); %entire orbit
+
+    title("Lambert Arc from Earth to Mars-Lillian Shido")
+    xlabel("Distance [km]")
+    ylabel("Distance [km]")
+    
+    % Keep zoom fixed
+    daspect([1 1 1]); %axis equal
+    h = zoom();
+    h.ActionPostCallback = @(o, e) daspect(e.Axes, [1 1 1]);
+end
