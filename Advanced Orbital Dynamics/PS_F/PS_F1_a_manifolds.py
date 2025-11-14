31c23913068724d56817a272e5bbe84d4e064da8
Lillian Shido <lillian.shido@gmail.com>
2025-11-13 23:47:12 -0800

    Added velocity eigenspace projection

---
+++ Advanced Orbital Dynamics/PS_F/PS_F1_a_manifolds.py
@@ -0,0 +1,120 @@
+ps = "F1 part a"
+# Author: Lillian Shido
+# Date: 11/13/2025
+
+import numpy as np
+from math import pi
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+from constants import mu_Earth, mu_Moon, a_Moon
+from methods import system_properties, calc_L1, build_A_matrix_collinear, spatial_ode
+
+import warnings
+warnings.filterwarnings("ignore",category=PendingDeprecationWarning)
+warnings.filterwarnings("ignore",category=FutureWarning)
+
+mu = mu_Moon/(mu_Earth + mu_Moon)
+
+# Properties of the system
+mu, l_char, t_char, x_Earth, x_Moon = system_properties(mu_Earth, mu_Moon, a_Moon)
+x_L1, y_L1 = calc_L1(mu, a_Moon)
+
+# Determine the stable and unstable manifolds 
+d_dim = 30 #[km]
+d_val = d_dim/l_char #[non-dim]
+print(d_val)
+tf = 1*pi
+pos_tspan = [0,tf]
+neg_tspan = [0,-tf]
+
+A = build_A_matrix_collinear(mu, x_L1, y_L1, 0)
+eigenvalues, eigenvectors = np.linalg.eig(A)
+# Plot eigenvectors
+fig1 = plt.figure()
+ax1 = fig1.add_subplot()
+ax1.scatter(x_L1, y_L1, s=25, label="L1", color="green")
+ax1.scatter(x_Moon, 0, s=30, label="Moon", color="gray")
+for i in range(0,6):
+    if i==0 or i==1:
+        print(f"vector {i+1} i-comp: {eigenvectors[:,i][0].real[0,0]}")
+        print(f"vector {i+1} y-comp: {eigenvectors[:,i][1].real[0,0]}")
+        if i==0:
+            label=f"$E^S$"
+            linestyle="solid"
+            label_point="$x_S^+$"
+            label_manifold="$W_{{loc}}^S$"
+            color="blue"
+        elif i==1:
+            label=f"$E^U$"
+            linestyle="dashed"
+            label_point="$x_U^+$"
+            label_manifold="$W_{{loc}}^U$"
+            color="red"
+        x_eig = x_L1 + eigenvectors[:,i][0].real[0,0]
+        y_eig = y_L1 + eigenvectors[:,i][1].real[0,0]
+        nu_W = eigenvectors[:,i]/np.linalg.norm(eigenvectors[:,i][0:3])
+        print(nu_W)
+        scaled_nu_W = d_val*nu_W
+        x0_pos = x_L1 + scaled_nu_W[0].real[0,0]
+        y0_pos = y_L1 + scaled_nu_W[1].real[0,0]
+        vx0_pos = scaled_nu_W[3].real[0,0]
+        vy0_pos = scaled_nu_W[4].real[0,0]
+        print(f"{x0_pos:.4f}")
+        print(f"{y0_pos:.4f}")
+        print(f"{vx0_pos:.4f}")
+        print(f"{vy0_pos:.4f}")
+        # check the distance from eq
+        check_distance = np.linalg.norm(np.array([[x0_pos-x_L1],[y0_pos-y_L1]]))*l_char
+        print(f"Check distance!: {check_distance}")
+        ax1.axline((x_L1,y_L1), (x_eig,y_eig), label=label,linestyle=linestyle)
+        # ax1.scatter(x0_pos, y0_pos, label=label_point, s=10,zorder=3)
+        IC_pos = [
+            x0_pos,y0_pos,0,vx0_pos,vy0_pos,0,# IC states
+            1,0,0,0,0,0, # Identity matrix for phi ICs
+            0,1,0,0,0,0,
+            0,0,1,0,0,0,
+            0,0,0,1,0,0,
+            0,0,0,0,1,0,
+            0,0,0,0,0,1
+        ]
+        pos_prop = solve_ivp(spatial_ode, pos_tspan, IC_pos, args=(mu,), rtol=1e-12,atol=1e-14)
+        ax1.plot(pos_prop.y[0], pos_prop.y[1],label=label_manifold+" +", color=color, linestyle=linestyle, zorder=2.5)
+        neg_prop = solve_ivp(spatial_ode, neg_tspan, IC_pos, args=(mu,), rtol=1e-12,atol=1e-14)
+        ax1.plot(neg_prop.y[0], neg_prop.y[1], color=color, linestyle=linestyle, zorder=2.5)
+        # Now do the other sides of the manifolds
+        x0_neg = x_L1 - scaled_nu_W[0].real[0,0]
+        y0_neg = y_L1 - scaled_nu_W[1].real[0,0]
+        vx0_neg = -scaled_nu_W[3].real[0,0]
+        vy0_neg = -scaled_nu_W[4].real[0,0]
+        IC_neg = [
+            x0_neg,y0_neg,0,vx0_neg,vy0_neg,0,# IC states
+            1,0,0,0,0,0, # Identity matrix for phi ICs
+            0,1,0,0,0,0,
+            0,0,1,0,0,0,
+            0,0,0,1,0,0,
+            0,0,0,0,1,0,
+            0,0,0,0,0,1
+        ]
+        pos_prop = solve_ivp(spatial_ode, pos_tspan, IC_neg, args=(mu,), rtol=1e-12,atol=1e-14)
+        ax1.plot(pos_prop.y[0], pos_prop.y[1],label=label_manifold+" -", color=color, linestyle=linestyle, zorder=2.5)
+        neg_prop = solve_ivp(spatial_ode, neg_tspan, IC_neg, args=(mu,), rtol=1e-12,atol=1e-14)
+        ax1.plot(neg_prop.y[0], neg_prop.y[1], color=color, linestyle=linestyle, zorder=2.5)
+
+
+plt.legend(ncol=3,framealpha=1)
+lim=1e-4
+ax1.axis('equal')
+# ax1.set(xlim=(-lim,lim),ylim=(-lim,lim))
+ax1.set_xlabel("x [non-dim]")
+ax1.set_ylabel("y [non-dim]")
+plt.title(f"Stable and Unstable Manifolds ({ps}, Lillian Shido)")
+plt.grid()
+plt.savefig(f'manifolds_{ps}.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+
+
+
+
+
--- Advanced Orbital Dynamics/PS_F/PS_F1_a_velocity.py
+++ Advanced Orbital Dynamics/PS_F/PS_F1_a_velocity.py
@@ -5,6 +5,7 @@ ps = "F1 part a"
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 from constants import mu_Earth, mu_Moon, a_Moon
 from methods import system_properties, calc_L1, build_A_matrix_collinear
@@ -36,11 +37,13 @@ print("\n".join([" ".join(f"{item:15.5f}" for item in row) for row in np.asarray
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot()
-ax1.scatter(x_L1, y_L1, s=20, label="L1", color="red")
+ax1.xaxis.set_major_locator(ticker.MultipleLocator(0.2))
+ax1.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+# ax1.scatter(x_L1, y_L1, s=20, label="L1", color="red")
 for x in range(0,6):
     if x==0 or x==1:
-        print(f"vector {x+1} x-comp: {eigenvectors[:,x][0].real[0,0]}")
-        print(f"vector {x+1} y-comp: {eigenvectors[:,x][1].real[0,0]}")
+        print(f"vector {x+1} x-comp: {eigenvectors[:,x][3].real[0,0]}")
+        print(f"vector {x+1} y-comp: {eigenvectors[:,x][4].real[0,0]}")
         if x==0:
             # label=f"Eigenvector {x+1} (Stable)"
             label=f"$E^S$"
@@ -51,16 +54,32 @@ for x in range(0,6):
             linestyle="dashed"
         x_eig = x_L1 + eigenvectors[:,x][0].real[0,0]
         y_eig = y_L1 + eigenvectors[:,x][1].real[0,0]
+        vx_eig = eigenvectors[:,x][3].real[0,0]
+        vy_eig = eigenvectors[:,x][4].real[0,0]
         # ax1.plot([x_L1,x_eig],[y_L1,y_eig], label=label, linewidth=1.5)
-        ax1.axline((x_L1,y_L1), (x_eig,y_eig), label=label,linestyle=linestyle)
+        # ax1.axline((x_L1,y_L1), (x_eig,y_eig), label=label+" pos",linestyle=linestyle)
+        ax1.axline((0,0), (vx_eig,vy_eig), label=label+" vel",linestyle=linestyle, color="green")
 plt.legend(framealpha=1)
 ax1.axis('equal')
-ax1.set_xlabel("x [non-dim]")
-ax1.set_ylabel("y [non-dim]")
+ax1.set_xlabel(r"$\dot{x}$"+"\n[non-dim]")
+ax1.set_ylabel(r"$\dot{y}$"+"\n[non-dim]")
+ax1.set(xlim=(-0.8,0.8))
 # plt.title(f"Stable and Unstable Eigenvectors ({ps}, Lillian Shido)")
-plt.title(f"Stable and Unstable Eigenspaces ({ps}, Lillian Shido)")
+plt.title(f"Stable and Unstable Eigenspaces Projected into the Velocity Space\n({ps}, Lillian Shido)")
 plt.grid()
 # plt.savefig(f'Eigenvectors_{ps}.png', dpi=300, bbox_inches='tight')
-plt.savefig(f'Eigenspaces_{ps}.png', dpi=300, bbox_inches='tight')
+plt.savefig(f'Eigenspaces_velocity{ps}.png', dpi=300, bbox_inches='tight')
+
+# Calc angle between the stable and unstable eigenvectors projected on the velocity space
+b_vec = eigenvectors[:,0][3:5]
+c_vec = eigenvectors[:,1][3:5]
+norm_b = np.linalg.norm(b_vec)
+norm_c = np.linalg.norm(c_vec)
+try:
+    dot = np.dot(np.asarray(b_vec).flatten(),np.asarray(c_vec).flatten()).real
+except:
+    pdb.set_trace()
+angle = np.rad2deg(np.arccos(dot/(norm_b*norm_c)))
+print(f"angle b/t Eig_1 and eig_2: {angle}")
 
 pdb.set_trace()
